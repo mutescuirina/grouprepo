62764c144ef32c74ac531f4742579f7922d2926a
acsirina <irina.a@irinas-MacBook-Pro.local>
2019-05-09 09:45:46 -0400

    created the second route

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require(express)
+const express = require('express')
 const app = express()
 const PORT = 3000
 
 app.get('/', (req, res) => {
     res.send("Hello World")
+})
+app.get('/zipcodes', (req,res )=>{
+    res.send('zipcodes rocks')
 })
 
 app.listen(PORT, () => {
