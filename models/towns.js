b63cf68e655989fb6ee93b3f9eb18e1c71130ac1
acsirina <irina.a@irinas-MacBook-Pro.local>
2019-05-09 13:56:05 -0400

    model buit

---
+++ models/towns.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose')
+
+const townsSchema = mongoose.Schema({
+    name:{type: String, required: true},
+    population:{type:Number},
+    coast: {type:Boolean, default: false}
+})
+module.exports = mongoose.model(Town, townsSchema )
