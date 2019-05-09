afa94a4dfebd51ff0704a66eedd5108756d61f50
acsirina <irina.a@irinas-MacBook-Pro.local>
2019-05-09 14:52:00 -0400

    delete & update route created

--- controllers/towns.js
+++ controllers/towns.js
@@ -3,11 +3,10 @@ const towns = express.Router()
 const Town = require('../models/towns')
 
 //Create Route
-
 towns.post('/', (req, res) => {
     Town.create(req.body, (error, createdTown) => {
-        if(error) {
-            res.status(400).json({error: error.message})
+        if (error) {
+            res.status(400).json({ error: error.message })
         }
         res.status(200).send(createdTown)
     })
@@ -16,10 +15,32 @@ towns.post('/', (req, res) => {
 //Index Route
 towns.get('/', (req, res) => {
     Town.find({}, (error, foundTowns) => {
-        if(error) {
-            res.status(400).json({error: error.message})
+        if (error) {
+            res.status(400).json({ error: error.message })
         }
         res.status(200).json(foundTowns)
+    })
+})
+
+//Delete
+towns.delete('/:id', (req, res) => {
+    Town.findByIdAndRemove(req.params.id, (error, deletedTown) => {
+        if (error) {
+            res.status(400).json({ error: error.message })
+        }
+        res.status(200).json(deletedTown)
+    })
+})
+
+//Town
+towns.put('/:id', (req, res) => {
+    Town.findByIdAndUpdate(req.params.id, req.body, {
+        new: true
+    }, (err, updatedTown) => {
+        if (err) {
+            res.status(400).json({ error: error.message })
+        }
+        res.status(200).json(updatedTown)
     })
 })
 
